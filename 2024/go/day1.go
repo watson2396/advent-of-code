541a0a21e4d6a504c4ecae8333d3f21a54a4f83a
Jacob Watson <JacobWatson@sbp.com>
2025-06-06 11:30:19 -0400

    feat: finally got first star of day1

--- 2024/go/day1.go
+++ 2024/go/day1.go
@@ -4,46 +4,54 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
 
-func main() {
-	var input_file = "day1_input-1.csv"
+func day1_star1() {
+	var input_file = "./day1_input.txt"
 
 	var lcol = []int{}
 	var rcol = []int{}
+	var totalDiff = 0
 
-	f, err := os.Open(input_file)
-	if err != nil {
-		fmt.Println("error opening file")
-	}
+	f, _ := os.Open(input_file)
 	defer f.Close()
 
 	reader := bufio.NewReader(f)
 
 	for {
-		line, err := reader.ReadString('n')
+		line, err := reader.ReadString('\n')
+		nums := strings.Split(line, "   ")
+
+		lnum, _ := strconv.Atoi(nums[0])
+		rnum, _ := strconv.Atoi(strings.TrimSuffix(nums[1], "\r\n"))
+
+		lcol = append(lcol, lnum)
+		rcol = append(rcol, rnum)
+
 		if err != nil {
 			if err.Error() == "EOF" {
 				break
 			}
 			fmt.Println("error reading file line by line")
 		}
-
-		nums := strings.Split(line, " ")
-
-		lnum, err := strconv.Atoi(nums[0])
-		if err != nil {
-			fmt.Println("error convert left number to integer")
-		}
-		lcol = append(lcol, lnum)
-
-		rnum, err := strconv.Atoi(nums[0])
-		if err != nil {
-			fmt.Println("error convert left number to integer")
-		}
-		rcol = append(rcol, rnum)
 	}
 
+	slices.Sort(lcol)
+	slices.Sort(rcol)
+
+	fmt.Println(len(lcol))
+
+	for i := 0; i < len(lcol); i++ {
+		var diff = lcol[i] - rcol[i]
+		if diff < 0 {
+			diff *= -1
+		}
+		//fmt.Printf("lcol: %d, rcol: %d, diff: %d\n", lcol[i], rcol[i], diff)
+		totalDiff += diff
+	}
+
+	fmt.Println("total diff", totalDiff)
 }
